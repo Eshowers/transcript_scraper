d43b02e3ee84b98a245c21db771cd69f5cab6615
ntindall <ntindall2@gmail.com>
2014-07-16 15:36:02 -0700

    Boilerplate with makefile

---
+++ build/release/scraper.d
@@ -0,0 +1,1 @@
+build/release/scraper.o: scraper.cpp
---
+++ scraper.cpp
@@ -0,0 +1,15 @@
+#include <climits>
+#include <getopt.h>
+#include <fstream>
+#include <set>
+//#include <socket++/sockstream.h>
+#include <thread>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+    cout << "Hello" << endl;
+}
